2d487161c8fcbee64f024da75b0fe24c6c83341d
asbabushkin <asbabushkin@yandex.ru>
2021-11-19 16:40:51 +0500

    Смена концепции. Данный коммит - финальное сохранение первой концепции.

--- web_store/store/static/store/js/script.js
+++ web_store/store/static/store/js/script.js
@@ -1,21 +1,13 @@
-alert('Script is working');
 let phoneArray = document.getElementsByClassName("dropdown-item");
 
-alert(phoneArray);
-alert(phoneArray.length);
-
 let choosePhone = function(){
-    alert('In function');
     let phoneId=this.getAttribute('id');
-    alert(phoneId);
-
 //    let phoneDiv = document.getElementById("phone-selected");
     document.getElementById("h2-phone-selected").innerHTML = "Чехлы для телефона " + phoneId;
-
+    document.getElementById("h2-phone-unselected").setAttribute(id="h2-phone-selected")
 //    let phoneNameLabel = document.createElement("h2");
 //    phoneNameLabel.innerHTML = "Чехлы для телефона " + phoneId;
 //    phoneDiv.append(phoneNameLabel);
-
 }
 
 for (let i = 0;i < phoneArray.length; i++){
