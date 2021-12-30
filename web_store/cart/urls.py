17b3dcb09ee0ecec4311e47eaffc7da219486506
asbabushkin <asbabushkin@yandex.ru>
2021-12-31 00:27:41 +0500

    Ajax is added to product.html Modal window is added to product.html

--- web_store/cart/urls.py
+++ web_store/cart/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
     url(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart_remove'),
     path('buy/', views.buy, name='buy'),
+    # path('modal-cart/', views.modal_window, name='cart_modal_window')
 ]
