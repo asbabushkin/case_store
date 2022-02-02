5ac7941ad130a2dd3e8d0a1ff6738a1ea6817f86
asbabushkin <asbabushkin@yandex.ru>
2022-02-02 16:19:54 +0500

    Adding pagination

--- web_store/store/views.py
+++ web_store/store/views.py
@@ -30,6 +30,7 @@ class StoreHome(ListView):
     form_filter = ProductFilterForm
     template_name = 'store/index.html'
     context_object_name = 'products'
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
