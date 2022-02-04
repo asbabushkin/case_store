6f76c524bf5ef76563dbe7bec32ff0d66196ae23
asbabushkin <asbabushkin@yandex.ru>
2022-02-04 13:50:03 +0500

    ready to deploy

--- web_store/orders/urls.py
+++ web_store/orders/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-
+from django.urls import path
 
 from . import views
 
 app_name = 'orders'
 urlpatterns = [
     url(r'^create/$', views.order_create, name='order_create'),
-
+    path('buy/', views.buy, name='buy'),
 ]
