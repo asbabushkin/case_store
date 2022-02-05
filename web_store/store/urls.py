8b8e46b969f52059e73f3432a1728aa53d94c884
asbabushkin <asbabushkin@yandex.ru>
2022-02-05 12:14:36 +0500

    Commit before cleaning code

--- web_store/store/urls.py
+++ web_store/store/urls.py
@@ -6,19 +6,18 @@ from .views import *
 app_name = 'store'
 urlpatterns = [
 
-    # path('', index, name='index'),
+
     path('', StoreHome.as_view(), name='index'),
     path('product/<slug:product_slug>', ProductPage.as_view(), name='product_page'),
-#    path('cart/', CartPage.as_view(), name='cart'),
-    path('about/', about, name='about'),
+    # path('about/', about, name='about'),
     path('about_delivery/', about_delivery, name='about_delivery'),
     path('about_payment/', about_payment, name='about_payment'),
     path('faq/', faq, name='faq'),
-    path('personal_account/', personal_account, name='personal_account'),
-    path('category/', category, name='category'),
-    path('phone/', phone, name='phone'),
-    path('login/', login, name='login'),
-    #path('case/<slug:product_slug>/', product_page, name='product_page'),
+    # path('personal_account/', personal_account, name='personal_account'),
+    # path('category/', category, name='category'),
+    # path('phone/', phone, name='phone'),
+    # path('login/', login, name='login'),
+
 
 
 ]
